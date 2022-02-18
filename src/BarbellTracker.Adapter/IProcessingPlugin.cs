76f169a61e267f175c4816a1467feb9323933bf8
Hawk1401 <schalke.koch@web.de>
2022-02-18 13:03:34 +0100

    Added new IProcessingPlugin Interface

---
+++ src/BarbellTracker.Adapter/IProcessingPlugin.cs
@@ -0,0 +1,20 @@
+﻿using BarbellTracker.ApplicationCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarbellTracker.Adapter
+{
+    internal interface IProcessingPlugin :IPlugin
+    {
+        public bool IsActiv();
+
+        public bool Activate();
+
+        public bool Deactivate();
+
+        public Task ProcessData(EventContext eventContext);
+    }
+}
