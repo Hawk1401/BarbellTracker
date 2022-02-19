a449c2989583f32db0bf39510137096f1b455575
Hawk1401 <schalke.koch@web.de>
2022-02-19 14:24:25 +0100

    Added Velocity Model

---
+++ src/BarbellTracker.Services/Models/Velocity.cs
@@ -0,0 +1,15 @@
+﻿using BarbellTracker.AbstractionCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarbellTracker.Services.Models
+{
+    public  class Velocity
+    {
+        public Vector2D[] Vectors { get; set; }
+        public int FPS { get; set; }
+    }
+}
