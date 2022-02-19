a3356b8be6aea72df55b5b842b7b99fd3e31fb77
Hawk1401 <schalke.koch@web.de>
2022-02-19 14:23:13 +0100

    Removed not needed methodes from IService Interface

--- src/BarbellTracker.Services/Interface/IService.cs
+++ src/BarbellTracker.Services/Interface/IService.cs
@@ -3,18 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BarbellTracker.Adapter.Interface;
 using BarbellTracker.ApplicationCode;
 
-namespace BarbellTracker.Services
+namespace BarbellTracker.Services.Interface
 {
     public interface IService
     {
-        public string  Name { get; set; }
-        public string Description { get; set; }
-
-        public bool Activate();
-        public bool Deactivate();
-
-        public Task HandleTracking(EventContext eventContext);
+        public string Name { get; }
+        public string Description { get; }
     }
 }
