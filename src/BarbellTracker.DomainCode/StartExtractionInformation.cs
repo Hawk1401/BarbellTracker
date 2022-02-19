eecdf9a28f685224a313686ff84e43c7b51fccb2
Hawk1401 <schalke.koch@web.de>
2022-02-19 14:16:46 +0100

    Added setting and StartEctraction entities  to Domain

---
+++ src/BarbellTracker.DomainCode/StartExtractionInformation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarbellTracker.DomainCode
+{
+    public class StartExtractionInformation
+    {
+        public string PluginName { get; set; }
+        public string Id => $"{ExtractionName}-{dateTime}";
+        public string ExtractionName { get; set; }
+
+        private string dateTime = DateTime.Now.ToString("yyyy.MM.dd--HH-mm-ss"); // maby we should use some ISO standard for the format
+
+    }
+}
