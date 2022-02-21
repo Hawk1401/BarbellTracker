7d0fb430947756942cd824e4c383dca6ae8a7174
JAN PHILLIP METZGER <jan-phillip.metzger@agilent.com>
2022-02-21 17:52:38 +0100

    added new view from one adapter

---
+++ src/BarbellTracker.WPF_DesktopClient/ViewModel/AdapterVelocityTableViewModel.cs
@@ -0,0 +1,31 @@
+﻿using BarbellTracker.Adapter.Model;
+using BarbellTracker.WPF_HelperClasses;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarbellTracker.WPF_DesktopClient.ViewModel
+{
+    internal class AdapterVelocityTableViewModel : PropertyChangedNotifier
+    {
+        private ObservableCollection<CSVVelocityModel> _csvVelocityModels = new ObservableCollection<CSVVelocityModel>();
+
+        public AdapterVelocityTableViewModel()
+        {
+
+        }
+
+        public ObservableCollection<CSVVelocityModel> CSVVelocityModels
+        {
+            get { return _csvVelocityModels; }
+            set
+            {
+                _csvVelocityModels = value;
+                OnPropertyChanged("CSVVelocityModels");
+            }
+        }
+    }
+}
