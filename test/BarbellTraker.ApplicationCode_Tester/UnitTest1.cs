8574eda3d0a143f6a28578598071f67ab6e2648d
florkoch <florian.koch@agilent.com>
2022-02-21 22:36:57 +0100

    Added Tests

---
+++ test/BarbellTraker.ApplicationCode_Tester/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BarbellTraker.ApplicationCode_Tester
+{
+    public class AdapterStorageTester
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
