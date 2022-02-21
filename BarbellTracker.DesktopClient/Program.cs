49f57d2144d8ac04686b2da048320f69f064b544
florkoch <florian.koch@agilent.com>
2022-02-21 11:34:21 +0100

    Added new Project

---
+++ BarbellTracker.DesktopClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BarbellTracker.Adapter;
+using BarbellTracker.Plugin.WPF_MVVM_UI;
+using BarbellTracker.Plugins;
+
+namespace BarbellTracker.DesktopClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pluginManager = PluginManager.Instance;
+            var uiAdapterManager = UIAdapterManager.Instance;
+
+            pluginManager.AddPlugin(new VelocityToTable());
+            uiAdapterManager.AddNewAdapter(new UIVideoAdapter());
+
+
+            var wpf = new MainWindow();
+        }
+    }
+}
