9dd6bee71ce534185debf25a14ee79858770a352
florkoch <florian.koch@agilent.com>
2022-03-02 13:30:13 +0100

    Moved Test Project into the test folder

--- test/BarbellTracker.ServicesTests/VelocityCSVTranslaterTester.cs
+++ test/BarbellTracker.ServicesTests/VelocityCSVTranslaterTester.cs
@@ -90,14 +90,14 @@ namespace BarbellTracker.ServicesTests
             var FirstVector = new Vector2D() { X = 0, Y = 1 };
             var SecondVector = new Vector2D() { X = 2, Y = 3 };
             var ThirdVector = new Vector2D() { X = 4, Y = 5 };
-            var FPS = 1;
+            var FPS = 30;
 
             var velocity = CreateVelocityObject(FPS, FirstVector, SecondVector, ThirdVector);
 
             var expected = new VectorCSVModel();
             expected.AddItem("00:00:", FirstVector.length(), FirstVector.ToString());
-            expected.AddItem("00:01:", SecondVector.length(), SecondVector.ToString());
-            expected.AddItem("00:02:", ThirdVector.length(), ThirdVector.ToString());
+            expected.AddItem("00:00:03", SecondVector.length(), SecondVector.ToString());
+            expected.AddItem("00:00:06", ThirdVector.length(), ThirdVector.ToString());
 
             var CSV = sut.CreateCSV(velocity);
 
